8bb6ad5c69bc99fcab1d69193f483ba1e358206a
sba23352 <sba23352@student.cct.ie>
2024-02-26 21:08:56 +0000

    New java class "employee" created

---
+++ src/ca2_sba23352/Employee.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ca2_sba23352;
+
+/**
+ *
+ * @author amber
+ */
+public class Employee {
+    
+}
