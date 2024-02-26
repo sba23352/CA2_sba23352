f82372cc3366636a63e924371d591ac4aae250c6
sba23352 <sba23352@student.cct.ie>
2024-02-26 21:45:10 +0000

    Static field set to 1 and incrementing each time inside constructor

--- src/ca2_sba23352/Employee.java
+++ src/ca2_sba23352/Employee.java
@@ -9,24 +9,29 @@ package ca2_sba23352;
  * @author amber
  */
 public class Employee {
+
     //Instance fields
     private String fullName;
     private String email;
     private int empNum;
 
-    
     //default value constructor
-    public Employee(){
+    public Employee() {
         this.fullName = "Indiana Jones";
         this.email = "Ijunior@gmail.com";
+        this.empNum = nextEmpNum++;
     }
-    
+
     //constructor with values passed as parameters
-    public Employee(String fullName ,String email) {
+    public Employee(String fullName, String email) {
         this.fullName = fullName;
         this.email = email;
+        this.empNum = nextEmpNum++;
     }
+
+    //Static field "nextEmpNum intialised at 1 and set to increment
+    private static int nextEmpNum = 1;
     
     
-    
+
 }
