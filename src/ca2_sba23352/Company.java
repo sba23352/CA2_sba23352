7b5a46ec6cba021f03831f64897e4e5865e60f32
sba23352 <sba23352@student.cct.ie>
2024-02-29 13:32:07 +0000

    Company class made with variable for company name and arraylist for employee data

---
+++ src/ca2_sba23352/Company.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ca2_sba23352;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author amber
+ */
+public class Company {
+    //String variable to hold name of company and ArrayList to hold list of employee objects
+    String companyName;
+    ArrayList<String> staff;
+}
+public Company() {
+
+}
+
